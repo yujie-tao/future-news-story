b7af96426140fcc6486a4ec9e1b2c096f7a520ea
yujie-tao <yujietao@outlook.com>
2018-07-23 23:18:15 +0800

    clean up code

--- js/voicecontrol.js
+++ js/voicecontrol.js
@@ -1,42 +1,5 @@
-(function() {
-  "use strict";
 
-  class Component {
-    printError(error) {
-      return console.log(error);
-    }
-
-    toggleСlass(node, className) {
-      return node.classList.toggle(className);
-    }
-  }
-
-  class Menu extends Component {
-    constructor(settings) {
-      super();
-      this.nodeMenu = settings.nodeMenu;
-      this.stateErrorHint =
-        settings.stateErrorHint || "activeClassName is not a string";
-    }
-
-    toggleState(stateClassName) {
-      if (typeof stateClassName !== "string")
-        return super.printError(this.stateErrorHint);
-
-      return super.toggleСlass(this.nodeMenu, stateClassName);
-    }
-  }
-
-  let nodeJSMenu = document.querySelector("body");
-
-  let demoMenu = new Menu({
-    nodeMenu: nodeJSMenu
-  });
-
-  nodeJSMenu
-    .querySelector(".js-menu__toggle")
-    .addEventListener("click", function() {
-    demoMenu.toggleState("js-menu_activated");
-  });
-})();
+$(".voice").click(function(){
+  $(".voice").css({"width": "264px","height":"562px","border-radius":"38px","transition": "transform .3s, width .3s, height .3s"});
+});
 
